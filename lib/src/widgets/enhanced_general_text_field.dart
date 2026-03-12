5d9bcf912229674f3aece4da644b1c8ef633e4cb
faiq <faiq@voltras.co.id>
2026-03-12 10:41:23 +0700

    save again

--- lib/src/widgets/enhanced_general_text_field.dart
+++ lib/src/widgets/enhanced_general_text_field.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:iv_project_core/iv_project_core.dart';
 import 'package:iv_project_widget_core/iv_project_widget_core.dart';
 import 'package:quick_dev_sdk/quick_dev_sdk.dart';
@@ -7,6 +8,10 @@ import 'package:quick_dev_sdk/quick_dev_sdk.dart';
 class EnhancedGeneralTextField extends StatefulWidget {
   const EnhancedGeneralTextField({
     super.key,
+    required this.fieldTextColor,
+    required this.fieldLabelColor,
+    required this.fieldFillColor,
+    required this.fieldBorderColor,
     required this.textEditingController,
     this.labelTextBuilder,
     this.inputFormatters,
@@ -18,6 +23,10 @@ class EnhancedGeneralTextField extends StatefulWidget {
     this.validator,
   });
 
+  final Color fieldTextColor;
+  final Color fieldLabelColor;
+  final Color fieldFillColor;
+  final Color fieldBorderColor;
   final TextEditingController textEditingController;
   final String Function()? labelTextBuilder;
   final List<TextInputFormatter>? inputFormatters;
@@ -35,28 +44,30 @@ class EnhancedGeneralTextField extends StatefulWidget {
 class _EnhancedGeneralTextFieldState extends State<EnhancedGeneralTextField> {
   @override
   Widget build(BuildContext context) {
+    final lang = context.read<LocaleCubit>().state.languageCode;
+
     return GeneralTextField(
       controller: widget.textEditingController,
       height: (widget.maxLines ?? 1) > 1 ? null : H.x3l,
       maxLines: widget.maxLines,
       maxLength: widget.maxLength,
       enabled: widget.enabled,
-      style: AppFonts.inter(color: Colors.grey.shade100, fontSize: FontSize.md),
+      style: AppFonts.inter(color: widget.fieldTextColor, fontSize: FontSize.md),
       decoration: FieldDecoration(
         labelText: widget.labelTextBuilder?.call(),
-        labelStyle: AppFonts.inter(color: Colors.grey.shade300, fontSize: FontSize.md),
+        labelStyle: AppFonts.inter(color: widget.fieldLabelColor, fontSize: FontSize.md),
         filled: true,
-        fillColor: Colors.grey.shade500.withValues(alpha: .3),
+        fillColor: widget.fieldFillColor,
         enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.grey.shade600, width: 1),
+          borderSide: BorderSide(color: widget.fieldBorderColor, width: 1),
           borderRadius: const .all(.circular(8)),
         ),
         disabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.grey.shade600, width: 1),
+          borderSide: BorderSide(color: widget.fieldBorderColor, width: 1),
           borderRadius: const .all(.circular(8)),
         ),
         focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.grey.shade600, width: 1),
+          borderSide: BorderSide(color: widget.fieldBorderColor, width: 1),
           borderRadius: const .all(.circular(8)),
         ),
         suffixIcons: () {
@@ -76,7 +87,12 @@ class _EnhancedGeneralTextFieldState extends State<EnhancedGeneralTextField> {
           final validator = widget.validator!(value);
           if (validator.isSuccess == false) return validator;
         }
-        if (value.isEmpty && widget.isMandatory) return SharedPersonalize.fieldCanNotEmpty();
+        if (value.isEmpty && widget.isMandatory) {
+          return SharedPersonalize.fieldCanNotEmpty(
+            message: lang == 'id' ? 'Tidak boleh kosong' : 'Cannot be empty',
+            textStyle: TextStyle(color: Colors.red.shade400),
+          );
+        }
         return .success();
       },
     );
