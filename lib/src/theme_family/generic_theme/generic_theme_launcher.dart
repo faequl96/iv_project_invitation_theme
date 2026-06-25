198e7669dd3a7d7562df1ec26c174e493ad4b151
faiqul <faiq@voltras.co.id>
2026-06-25 16:23:41 +0700

    save

--- lib/src/theme_family/generic_theme/generic_theme_launcher.dart
+++ lib/src/theme_family/generic_theme/generic_theme_launcher.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:iv_project_invitation_theme/iv_project_invitation_theme.dart';
 import 'package:iv_project_invitation_theme/src/theme_family/generic_theme/generic_theme.dart';
-import 'package:iv_project_invitation_theme/src/theme_family/generic_theme/themes/black_and_white_glass.dart';
-import 'package:iv_project_invitation_theme/src/theme_family/generic_theme/themes/blue_sky_glass.dart';
 import 'package:iv_project_invitation_theme/src/theme_family/generic_theme/themes/floral_canvas.dart';
-import 'package:iv_project_invitation_theme/src/theme_family/generic_theme/themes/green_matcha_glass.dart';
-import 'package:iv_project_invitation_theme/src/theme_family/generic_theme/themes/javanese_rose_gold.dart';
-import 'package:iv_project_invitation_theme/src/theme_family/generic_theme/themes/red_velvet_glass.dart';
 import 'package:iv_project_model/iv_project_model.dart';
 
 class GenericThemeLauncher extends StatelessWidget {
@@ -33,13 +28,8 @@ class GenericThemeLauncher extends StatelessWidget {
   Widget build(BuildContext context) {
     final content = GenericTheme(
       configs: switch (invitationThemeId) {
-        1 => BlackAndWhiteGlass.configs,
-        2 => RedVelvetGlass.configs,
-        3 => GreenMatchaGlass.configs,
-        4 => BlueSkyGlass.configs,
-        5 => JavaneseRoseGold.configs,
         6 => FloralCanvas.configs,
-        int() => JavaneseRoseGold.configs,
+        int() => FloralCanvas.configs,
       },
       heightAdjustment: heightAdjustment,
       viewType: viewType,
@@ -50,16 +40,9 @@ class GenericThemeLauncher extends StatelessWidget {
     );
 
     final themeData = switch (invitationThemeId) {
-      1 => BlackAndWhiteGlass.themeData,
-      2 => RedVelvetGlass.themeData,
-      3 => GreenMatchaGlass.themeData,
-      4 => BlueSkyGlass.themeData,
-      5 => JavaneseRoseGold.themeData,
       6 => FloralCanvas.themeData,
-      int() => JavaneseRoseGold.themeData,
+      int() => FloralCanvas.themeData,
     };
-
-    if (themeData == null) return content;
 
     return Theme(data: themeData, child: content);
   }
--- lib/src/theme_family/generic_theme/generic_theme_launcher_as_image.dart
+++ lib/src/theme_family/generic_theme/generic_theme_launcher_as_image.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:iv_project_invitation_theme/src/theme_family/generic_theme/generic_theme_as_image.dart';
-import 'package:iv_project_invitation_theme/src/theme_family/generic_theme/themes/black_and_white_glass.dart';
-import 'package:iv_project_invitation_theme/src/theme_family/generic_theme/themes/blue_sky_glass.dart';
 import 'package:iv_project_invitation_theme/src/theme_family/generic_theme/themes/floral_canvas.dart';
-import 'package:iv_project_invitation_theme/src/theme_family/generic_theme/themes/green_matcha_glass.dart';
-import 'package:iv_project_invitation_theme/src/theme_family/generic_theme/themes/javanese_rose_gold.dart';
-import 'package:iv_project_invitation_theme/src/theme_family/generic_theme/themes/red_velvet_glass.dart';
 import 'package:iv_project_model/iv_project_model.dart';
 
 class GenericThemeLauncherAsImage extends StatelessWidget {
@@ -28,13 +23,8 @@ class GenericThemeLauncherAsImage extends StatelessWidget {
   Widget build(BuildContext context) {
     final content = GenericThemeAsImage(
       configs: switch (invitationThemeId) {
-        1 => BlackAndWhiteGlass.configs,
-        2 => RedVelvetGlass.configs,
-        3 => GreenMatchaGlass.configs,
-        4 => BlueSkyGlass.configs,
-        5 => JavaneseRoseGold.configs,
         6 => FloralCanvas.configs,
-        int() => JavaneseRoseGold.configs,
+        int() => FloralCanvas.configs,
       },
       initialPage: initialPage,
       useWrapper: useWrapper,
@@ -43,16 +33,9 @@ class GenericThemeLauncherAsImage extends StatelessWidget {
     );
 
     final themeData = switch (invitationThemeId) {
-      1 => BlackAndWhiteGlass.themeData,
-      2 => RedVelvetGlass.themeData,
-      3 => GreenMatchaGlass.themeData,
-      4 => BlueSkyGlass.themeData,
-      5 => JavaneseRoseGold.themeData,
       6 => FloralCanvas.themeData,
-      int() => JavaneseRoseGold.themeData,
+      int() => FloralCanvas.themeData,
     };
-
-    if (themeData == null) return content;
 
     return Theme(data: themeData, child: content);
   }
